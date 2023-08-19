2f9c8b7502a86c6856f4904dd8437b3e49eee25b
permana017 <ahdipermana07@gmail.com>
2023-08-19 10:32:22 +0700

    add: add set up daisyui

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
+  <div class="flex flex-col items-center">
     <p class="text-red-500 text-2xl font-bold text-center mt-10">Hallo QL Team</p>
+    <button class="btn btn-primary max-w-xl mt-5">Click Me</button>
   </div>
 </template>
 <script>
