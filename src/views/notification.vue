edecf12c9a5f009eb0dce7b17a5fc5792d466dde
permana017 <ahdipermana07@gmail.com>
2023-11-23 20:23:55 +0700

    test commit

--- src/views/notification.vue
+++ src/views/notification.vue
@@ -4,7 +4,12 @@
       <p>Notification Pagesdasdsad</p>
       <p>Name : {{ form.name }}</p>
       <p>Email: {{ form.email }}</p>
+      <p>Alamat: {{ form.email }}</p>
       <div>
+        <FormField label="Grouped with icons">
+          <FormControl v-model="form.name" :icon="mdiAccount" />
+          <FormControl v-model="form.email" type="email" :icon="mdiMail" />
+        </FormField>
         <FormField label="Grouped with icons">
           <FormControl v-model="form.name" :icon="mdiAccount" />
           <FormControl v-model="form.email" type="email" :icon="mdiMail" />
@@ -15,14 +20,14 @@
   />
 </template>
 <script setup>
-import { reactive } from 'vue';
-import LayoutMain from '../layouts/LayoutMain.vue';
-import { mdiAccount, mdiMail } from '@mdi/js';
-import FormControl from '../components/FormControl.vue';
-import FormField from '../components/FormField.vue';
+import { reactive } from "vue";
+import LayoutMain from "../layouts/LayoutMain.vue";
+import { mdiAccount, mdiMail } from "@mdi/js";
+import FormControl from "../components/FormControl.vue";
+import FormField from "../components/FormField.vue";
 
 const form = reactive({
-  name: 'ahdi',
-  email: '',
+  name: "ahdi",
+  email: "",
 });
 </script>
