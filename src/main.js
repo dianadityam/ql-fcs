e297ae0fab32825a182379ea0db82bf2bf8b8055
permana017 <ahdipermana07@gmail.com>
2023-11-25 01:54:45 +0700

    set global component

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import { createApp } from 'vue'
 import './style.css';
+import { globalcomponent } from "./plugin/global-component"
 import App from './App.vue';
 import router from './router';
 import { createPinia } from 'pinia'
 
 const pinia = createPinia()
 const app = createApp(App)
+globalcomponent(app);
 app.use(router)
 app.use(pinia)
 app.mount('#app')
