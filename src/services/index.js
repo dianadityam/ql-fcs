74bffc1ed788c5aee9940103b4c32d8f7b4307a4
Dian Aditya <dian.aditya53@gmail.com>
2023-11-25 01:41:26 +0700

    setup api services

---
+++ src/services/index.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+const getAuthToken = (options) => {
+  let result = {};
+  const dataUser = localStorage.getItem('authToken');
+  if (dataUser) {
+    result = {
+      Authorization: 'Bearer ' + dataUser,
+    };
+  }
+  result['Content-Type'] = options.formData ? 'multipart/form-data' : 'application/json';
+  return result;
+};
+
+const serviceApi = async (options) => {
+  let axiosConfig = {
+    baseURL: 'https://server.qqltech.com:7001/',
+    timeout: 100000,
+  };
+
+  axiosConfig.headers = getAuthToken(options);
+  const instance = axios.create(axiosConfig);
+  return await instance(options)
+    .then((res) => {
+      return { response: res.data, status: res.status };
+    })
+    .catch((err) => {
+      return err.response;
+    });
+};
+
+export default serviceApi;
