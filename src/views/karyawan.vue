fa2a89db0675c7c5ecac41c8e4d39bedc615254c
Dian Aditya <dian.aditya53@gmail.com>
2023-11-24 15:12:24 +0700

    create form for master karyawan

---
+++ src/views/karyawan.vue
@@ -0,0 +1,100 @@
+<template>
+  <LayoutMain>
+    <div class="px-9">
+      <h1>Master Karyawan Form</h1>
+      <div class="content-section">
+        <strong>Master Karyawan Header</strong>
+        <p class="font-bold text-red-600">New Data</p>
+        <div class="grid grid-cols-2 my-5">
+          <FormField label="Nama">
+            <FormControl />
+          </FormField>
+          <FormField label="NIK">
+            <FormControl />
+          </FormField>
+          <FormField label="Tempat Tgl Lahir">
+            <FormControl />
+            <FormControl />
+          </FormField>
+          <FormField label="No ID">
+            <FormControl />
+          </FormField>
+          <FormField label="Divisi">
+            <FormControl />
+          </FormField>
+          <FormField label="Tanggal Masuk">
+            <FormControl />
+          </FormField>
+          <FormField label="Training">
+            <FormControl />
+          </FormField>
+          <FormField label="Tanggal Selesai">
+            <FormControl />
+          </FormField>
+          <FormField label="Jenis Kelamin">
+            <FormControl />
+          </FormField>
+          <FormField label="Agama">
+            <FormControl />
+          </FormField>
+          <FormField label="Alamat KTP">
+            <FormControl />
+          </FormField>
+          <FormField label="RT / RW">
+            <FormControl />
+          </FormField>
+          <FormField label="Provinsi">
+            <FormControl />
+          </FormField>
+          <FormField label="Kota">
+            <FormControl />
+          </FormField>
+          <FormField label="Kecamatan">
+            <FormControl />
+          </FormField>
+        </div>
+        <div class="grid grid-cols-2">
+          <FormField label="Alamat Domisili">
+            <FormControl />
+          </FormField>
+          <FormField label="RT / RW">
+            <FormControl />
+          </FormField>
+          <FormField label="Provinsi">
+            <FormControl />
+          </FormField>
+          <FormField label="Kota">
+            <FormControl />
+          </FormField>
+          <FormField label="Kecamatan">
+            <FormControl />
+          </FormField>
+          <FormField label="Email">
+            <FormControl />
+          </FormField>
+          <FormField label="Foto">
+            <FormControl />
+          </FormField>
+          <FormField label="Phone">
+            <FormControl />
+          </FormField>
+          <FormField label="Status">
+            <FormControl />
+          </FormField>
+          <FormField>
+            <FormControl />
+          </FormField>
+          <FormField label="Catatan">
+            <FormControl />
+          </FormField>
+        </div>
+      </div>
+    </div>
+  </LayoutMain>
+</template>
+
+<script setup>
+import LayoutMain from '@/layouts/LayoutMain.vue';
+import FormField from '@/components/FormField.vue';
+import FormControl from '@/components/FormControl.vue';
+</script>
