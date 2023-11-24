75de528c1c911f38043f5abc0b3897d3292282d1
permana017 <ahdipermana07@gmail.com>
2023-11-25 01:55:16 +0700

    page pricelist supplier

---
+++ src/master/purchasing/pricelist-supplier.vue
@@ -0,0 +1,24 @@
+<template>
+  <LayoutMain>
+    <div class="px-5">
+      <p class="font-medium text-lg mb-3">Form Pricelist Supplier</p>
+      <div class="bg-white rounded-lg p-5 h-full">
+        <p class="font-medium mb-3">Pricelist Supplier Header</p>
+        <p class="font-medium text-red-400">New Data</p>
+        <div class="flex items-center w-full gap-14">
+          <p class="font-medium">Supplier</p>
+          <div class="w-full max-w-lg">
+            <Input
+              placeholder="SUP0001"
+              :icon="mdiMagnify"
+              class="bg-slate-400"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </LayoutMain>
+</template>
+<script setup>
+import { mdiMagnify } from "@mdi/js";
+</script>
