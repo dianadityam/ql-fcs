87e92152f2913df7c8ad40b091d731681c2c64d4
permana017 <ahdipermana07@gmail.com>
2023-11-27 20:45:28 +0700

    feat pricelist supplier

--- src/plugin/global-component.js
+++ src/plugin/global-component.js
@@ -9,9 +9,17 @@ const Input = defineAsyncComponent(() =>
 const InputField = defineAsyncComponent(() =>
     import("../components/FormField.vue"),
 );
+const Btn = defineAsyncComponent(() =>
+    import("../components/BaseBtn.vue"),
+);
+const mdicon = defineAsyncComponent(() =>
+    import("../components/BaseIcon.vue"),
+);
 
 export const globalcomponent = (app) => {
     app.component("LayoutMain", LayoutMain);
     app.component("Input", Input);
     app.component("InputField", InputField);
+    app.component("Btn", Btn);
+    app.component("mdicon", mdicon);
 };
