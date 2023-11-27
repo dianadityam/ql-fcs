944fa834164fd2246d6e128ed62cb83a11577390
Dian Aditya <dian.aditya53@gmail.com>
2023-11-28 02:05:05 +0700

    create datatable component

--- src/components/TableData.vue
+++ src/components/TableData.vue
@@ -1,21 +1,54 @@
 <template>
-  <DataTable :data="data" :columns="columns" :table-header="tableHeader" class="display">
+  <DataTable
+    :data="data"
+    :columns="columns"
+    :table-header="tableHeader"
+    :options="tableOptions"
+    class="stripe pageResize"
+    ref="table"
+  >
     <thead>
       <tr>
+        <th v-if="checkable" />
         <th v-for="item in tableHeader" :key="item.id">{{ item.title }}</th>
       </tr>
     </thead>
+    <tbody>
+      <tr v-for="item in data" :key="item.id">
+        <td>
+          {{ item }}
+        </td>
+      </tr>
+    </tbody>
   </DataTable>
 </template>
 
 <script setup>
 import DataTable from 'datatables.net-vue3';
 import DataTablesCore from 'datatables.net';
+import 'datatables.net-select';
+import 'datatables.net-responsive';
+import { onMounted, ref } from 'vue';
+
+let dt;
+const table = ref(null);
+
+const emit = defineEmits(['get-row-data']);
+
+onMounted(() => {
+  dt = table.value.dt;
+});
 
 defineProps({
   tableHeader: {
     type: Array,
     required: true,
+  },
+  tableOptions: {
+    type: Object,
+  },
+  data: {
+    type: Array,
   },
 });
 
