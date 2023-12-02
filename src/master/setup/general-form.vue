db7108b551f7a4c7f13714d34f439232c6afc215
Dian Aditya <dian.aditya53@gmail.com>
2023-12-02 23:03:32 +0700

    create master general page

---
+++ src/master/setup/general-form.vue
@@ -0,0 +1,111 @@
+<template>
+  <LayoutMain>
+    <div class="px-9">
+      <h1>Master General</h1>
+      <div class="content-section">
+        <strong>Master General Header</strong>
+        <p class="font-bold text-red-600 mt-5">New Data</p>
+        <div class="grid my-5 w-2/4">
+          <InputField label="Kode" required>
+            <Input v-model="form.kode" :options="userOptions" />
+          </InputField>
+          <InputField label="Deskripsi">
+            <Input v-model="form.deskripsi" type="textarea" :options="warehouseOptions" />
+          </InputField>
+          <InputField label="Group">
+            <Input v-model="form.group" :options="groupOptions" />
+          </InputField>
+          <InputField label="Status">
+            <Input v-model="form.status" type="select" :options="statusOptions" />
+          </InputField>
+        </div>
+        <div class="flex justify-between border-t-2 py-3">
+          <div>
+            <p>Created by</p>
+          </div>
+          <div>
+            <Button
+              type="submit"
+              color="success"
+              label="Save"
+              @click="onSubmit"
+              :class="{ 'opacity-25': form.processing }"
+              :disabled="form.processing"
+              small
+            />
+            <router-link to="/master/general">
+              <Button
+                type="submit"
+                color="white"
+                label="Cancel"
+                class="ml-2"
+                :class="{ 'opacity-25': form.processing }"
+                :disabled="form.processing"
+                small
+              />
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </LayoutMain>
+</template>
+
+<script setup>
+import service from '@/services';
+import Button from '@/components/Button.vue';
+import { reactive, watch } from 'vue';
+
+const onSubmit = () => {
+  const dataToSubmit = {
+    ...form,
+    group: form.group.label,
+    is_active: form.status.id,
+  };
+  console.log(dataToSubmit);
+  service({
+    method: 'POST',
+    url: '/operation/m_general',
+    token: true,
+    data: dataToSubmit,
+  });
+};
+
+const statusOptions = [
+  { id: 0, label: 'INAKTIF' },
+  { id: 1, label: 'AKTIF' },
+];
+
+const groupOptions = [
+  { id: 1, label: 'VEHICLE TYPE' },
+  { id: 2, label: 'BANK' },
+];
+
+const form = reactive({
+  kode: '',
+  deskripsi: '',
+  group: groupOptions[0],
+  status: statusOptions[0],
+});
+
+// watch(
+//   () => form.sesuai_ktp,
+//   (newValue) => {
+//     if (newValue) {
+//       form.alamat_d = form.alamat_ktp;
+//       form.rt_rw_d = form.rt_rw_ktp;
+//       form.provinsi_d = form.provinsi_ktp;
+//       form.kota_d = form.kota_ktp;
+//       form.kecamatan_d = form.kecamatan_ktp;
+//     } else {
+//       form.alamat_d = '';
+//       form.rt_rw_d = '';
+//       form.provinsi_d = '';
+//       form.kota_d = '';
+//       form.kecamatan_d = '';
+//     }
+//   }
+// );
+</script>
+
+<style></style>
