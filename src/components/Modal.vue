32540cc40f14c7f75b633cf68a48908f905670b0
Dian Aditya <dian.aditya53@gmail.com>
2023-12-05 18:51:48 +0700

    change button style modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -6,40 +6,30 @@
     class="overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center flex"
   >
     <div class="relative w-4/5 my-6 mx-auto max-w-7xl">
-      <!--content-->
       <div
         class="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none"
         ref="target"
       >
-        <!--header-->
         <div
           class="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t"
         >
           <slot name="header">default header</slot>
         </div>
-        <!--body-->
         <div class="relative p-6 flex-auto">
           <slot name="content">default body</slot>
         </div>
-        <!--footer-->
         <div
           class="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b"
         >
-          <!-- <slot name="footer">default footer</slot> -->
-          <button
-            class="text-red-500 bg-transparent border border-solid border-red-500 hover:bg-red-500 hover:text-white active:bg-red-600 font-bold uppercase text-sm px-6 py-3 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-            type="button"
-            @click="isShowModal"
-          >
-            Close
-          </button>
-          <button
-            class="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-            type="button"
+          <Button type="submit" color="white" label="Cancel" @click="isShowModal" small />
+          <Button
+            type="submit"
+            color="success"
+            :label="submitLabel"
             @click="submit"
-          >
-            {{ submitLabel }}
-          </button>
+            class="ml-2"
+            small
+          />
         </div>
       </div>
     </div>
@@ -49,6 +39,7 @@
 
 <script setup>
 import { ref } from 'vue';
+import Button from './Button.vue';
 
 defineProps({
   showModal: {
