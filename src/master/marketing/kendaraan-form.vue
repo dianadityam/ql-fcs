ae976029a0b16a60f303979608b0e48ea64edd14
Dian Aditya <dian.aditya53@gmail.com>
2023-12-11 17:24:22 +0700

    create kendaraan header and form

--- src/master/marketing/kendaraan-form.vue
+++ src/master/marketing/kendaraan-form.vue
@@ -1,5 +1,102 @@
 <template>
-  <div>Kendaraan</div>
+  <LayoutMain>
+    <div class="px-9">
+      <h1>Form Warehouse</h1>
+      <div class="content-section">
+        <strong>Warehouse Header</strong>
+        <p class="font-bold text-red-600 mt-5">New Data</p>
+        <div class="grid my-5 w-2/4">
+          <InputField label="Kode" required>
+            <Input v-model="form.kode" :options="userOptions" />
+          </InputField>
+          <InputField label="Merk Kendaraan" required>
+            <Input v-model="form.merk" />
+          </InputField>
+          <InputField label="Kapasitas Kendaraan">
+            <Input v-model="form.kapasitas" />
+          </InputField>
+          <InputField label="Plat Nomor Kendaraan">
+            <Input v-model="form.plat_no" />
+          </InputField>
+          <InputField label="Note">
+            <Input v-model="form.note" />
+          </InputField>
+          <InputField label="Status">
+            <Input v-model="form.status" type="select" :options="statusOptions" />
+          </InputField>
+        </div>
+        <div class="flex justify-between border-t-2 py-3">
+          <div>
+            <p>Created by</p>
+          </div>
+          <div>
+            <Button
+              type="submit"
+              color="success"
+              label="Save"
+              @click="onSubmit"
+              :class="{ 'opacity-25': form.processing }"
+              :disabled="form.processing || !isFormValid"
+              small
+            />
+            <Button
+              type="submit"
+              color="white"
+              label="Cancel"
+              @click="$router.push('/master/kendaraan')"
+              class="ml-2"
+              :class="{ 'opacity-25': form.processing }"
+              :disabled="form.processing"
+              small
+              outline
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </LayoutMain>
 </template>
 
-<script></script>
+<script setup>
+import LayoutMain from '@/layouts/LayoutMain.vue';
+import service from '@/services';
+import Button from '@/components/Button.vue';
+import { reactive, computed } from 'vue';
+
+const onSubmit = () => {
+  const dataToSubmit = {
+    ...form,
+    is_active: form.status.id,
+  };
+  console.log(dataToSubmit);
+  service({
+    method: 'POST',
+    url: '/operation/m_kend',
+    token: true,
+    data: dataToSubmit,
+  });
+};
+
+const statusOptions = [
+  { id: 0, label: 'INAKTIF' },
+  { id: 1, label: 'AKTIF' },
+];
+
+const form = reactive({
+  kode: '',
+  merk: '',
+  kapasitas: '',
+  plat_no: '',
+  note: '',
+  status: statusOptions[0],
+});
+
+const isFormValid = computed(() => {
+  return (
+    form.kode.trim() !== '' &&
+    form.merk.trim() !== '' &&
+    form.kapasitas.trim() !== '' &&
+    form.plat_no.trim() !== ''
+  );
+});
+</script>
--- src/master/marketing/kendaraan.vue
+++ src/master/marketing/kendaraan.vue
@@ -1,5 +1,68 @@
 <template>
-  <div>Kendaraan</div>
+  <LayoutMain>
+    <div class="px-9 min-h-screen">
+      <h1>Kendaraan List</h1>
+      <div class="content-section">
+        <router-link to="/form/kendaraan">
+          <Button class="py-2" color="info" outline label="Create New" small />
+        </router-link>
+        <div class="my-5">
+          <TableData
+            :data="kendaraan"
+            :columns="columns"
+            :tableHeader="tableHeader"
+            :tableOptions="tableOptions"
+          />
+        </div>
+      </div>
+    </div>
+  </LayoutMain>
 </template>
 
-<script></script>
+<script setup>
+import LayoutMain from '@/layouts/LayoutMain.vue';
+import TableData from '@/components/TableData.vue';
+import Button from '@/components/Button.vue';
+import { ref, onMounted } from 'vue';
+import service from '@/services';
+
+const tableOptions = {
+  responsive: true,
+  processing: true,
+};
+
+const tableHeader = [
+  { id: 1, title: 'No.' },
+  { id: 2, title: 'Kode' },
+  { id: 3, title: 'Merk Kendaraan' },
+  { id: 4, title: 'Kapasitas' },
+  { id: 5, title: 'Nopol' },
+  { id: 6, title: 'Status' },
+];
+
+const kendaraan = ref(null);
+
+const columns = [
+  { width: '5%', data: 'id' },
+  { data: 'kode' },
+  { data: 'merk' },
+  { data: 'kapasitas' },
+  { data: 'plat_no' },
+  { data: 'is_active', render: (data) => (data ? 'Aktif' : 'Inaktif') },
+];
+
+const fetchData = async () => {
+  const result = await service({
+    method: 'GET',
+    url: 'operation/m_kend',
+    token: true,
+  });
+  if (result.status === 200) {
+    kendaraan.value = result.response.data;
+  }
+};
+
+onMounted(() => {
+  fetchData();
+});
+</script>
