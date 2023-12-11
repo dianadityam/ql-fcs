3ecad7a0120230cda90471e5d68d045d8cc3bcea
Dian Aditya <dian.aditya53@gmail.com>
2023-12-11 17:24:36 +0700

    ekspedisi page

--- src/master/marketing/ekspedisi.vue
+++ src/master/marketing/ekspedisi.vue
@@ -1,5 +1,70 @@
 <template>
-  <div>Ekspedisi</div>
+  <LayoutMain>
+    <div class="px-9 min-h-screen">
+      <h1>Ekspedisi List</h1>
+      <div class="content-section">
+        <router-link to="/form/ekspedisi">
+          <Button class="py-2" color="info" outline label="Create New" small />
+        </router-link>
+        <div class="my-5">
+          <TableData
+            :data="ekspedisi"
+            :columns="columns"
+            :tableHeader="tableHeader"
+            :tableOptions="tableOptions"
+          />
+        </div>
+      </div>
+    </div>
+  </LayoutMain>
 </template>
 
-<script></script>
+<script setup>
+import LayoutMain from '@/layouts/LayoutMain.vue';
+import TableData from '@/components/TableData.vue';
+import Button from '@/components/Button.vue';
+import { ref, onMounted } from 'vue';
+import service from '@/services';
+
+const tableOptions = {
+  responsive: true,
+  processing: true,
+};
+
+const tableHeader = [
+  { id: 1, title: 'No.' },
+  { id: 2, title: 'Kode' },
+  { id: 3, title: 'Nama' },
+  { id: 4, title: 'Cabang' },
+  { id: 5, title: 'Contact Person' },
+  { id: 6, title: 'No. Telepon' },
+  { id: 7, title: 'Status' },
+];
+
+const ekspedisi = ref(null);
+
+const columns = [
+  { width: '5%', data: 'id' },
+  { data: 'kode' },
+  { data: 'nama' },
+  { data: 'cabang' },
+  { data: 'cp' },
+  { data: 'no_telp' },
+  { data: 'is_active', render: (data) => (data ? 'Aktif' : 'Inaktif') },
+];
+
+const fetchData = async () => {
+  const result = await service({
+    method: 'GET',
+    url: 'operation/m_ekspedisi',
+    token: true,
+  });
+  if (result.status === 200) {
+    ekspedisi.value = result.response.data;
+  }
+};
+
+onMounted(() => {
+  fetchData();
+});
+</script>
